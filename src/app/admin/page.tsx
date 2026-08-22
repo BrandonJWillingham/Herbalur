889317c7d58c3852e3349ded3d04bd28f30e4a21
Brandon J Willingham <brandonjwillingham@gmail.com>
2026-08-22 13:40:21 -0400

    updated dashboard import

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,11 @@
-// // app/admin/page.tsx
+// app/admin/page.tsx
 
-// import Dashboard from "@/components/sections/admin/dashboard";
+import Dashboard from "@/components/sections/admin/Dashboard";
 
-// export default function AdminPage() {
-//   return (
-//     <main className="container mx-auto py-10">
-//       <Dashboard />
-//     </main>
-//   );
-// }
+export default function AdminPage() {
+  return (
+    <main className="container mx-auto py-10">
+      <Dashboard />
+    </main>
+  );
+}
